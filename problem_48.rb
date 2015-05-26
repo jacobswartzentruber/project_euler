06508f809ef6817e022b60723309b4636ae31246
jacobswartentruber <jsswart@gmail.com>
2015-05-26 13:13:00 -0500

    Added answer to problem_48

--- problem_48.rb
+++ problem_48.rb
@@ -2,9 +2,13 @@
 #Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
 def power_powers(digit)
+	#Add all n^n numbers in sequence to get answers
 	answer = (1..digit).inject {|sum, num| sum + num**num}
+	#Output last 10 digits of answer
 	puts answer.to_s[-10..-1]
 end
 
+#Test with known value
 power_powers(10)
 power_powers(1000)
+#Returns 9110846700
